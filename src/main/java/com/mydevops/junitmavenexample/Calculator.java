74f1072b5a144948c260ae6252daa4a1db8415dc
cabobadilla <cbobadilla.dsoft@gmail.com>
2016-04-02 12:25:35 -0500

    primer commit

--- src/main/java/com/mydevops/junitmavenexample/Calculator.java
+++ src/main/java/com/mydevops/junitmavenexample/Calculator.java
@@ -3,7 +3,7 @@ package com.mydevops.junitmavenexample;
 public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
-		return a + b + 1;
+		return a + b;
 	}
 
 	public int subtraction(int a, int b) {
